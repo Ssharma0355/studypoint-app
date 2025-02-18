3d8be5bbd0fdad44d26932ab9fdd59d23937fba8
sachin_sde <ssachin.s@myclnq.co.in>
2025-02-18 19:40:05 +0530

    pdf

--- src/App.js
+++ src/App.js
@@ -10,17 +10,19 @@ import { Footer, Header } from "./components";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/studymaterial" element={<StudyMaterialPage />} />
-        <Route path="/presidents" element={<Presidents />} />
-        <Route path="/primeminsiter" element={<PrimeMinisters />} />
-        <Route path="/emperors" element={<Emperors />} />
-      </Routes>
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/studymaterial" element={<StudyMaterialPage />} />
+          <Route path="/presidents" element={<Presidents />} />
+          <Route path="/primeminsiter" element={<PrimeMinisters />} />
+          <Route path="/emperors" element={<Emperors />} />
+        </Routes>
+      </BrowserRouter>
       <Footer />
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/components/StudyMaterial.js
+++ src/components/StudyMaterial.js
@@ -1,23 +1,54 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./StudyMaterial.css"; // Link to the CSS file for styling
+import pdf from "../assets/ppf.pdf"; // Import the PDF file correctly
 
 function StudyMaterial() {
+  const downloadPdf = (filePath, fileName) => {
+    const link = document.createElement("a");
+    link.href = filePath;
+    link.setAttribute("download", fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="study-material main-page">
-      <h2>Study Material</h2>
-      <ul>
-        <li>
-          <Link to="/primeminsiter">Prime Minister of India</Link>
-        </li>
-        <li>
-          <Link to="/presidents">Presidents of India</Link>
-        </li>{" "}
-        <li>
-          <Link to="/emperors">Emperors of India</Link>
-        </li>
-        {/* New Link */}
-      </ul>
+      <div className="material-links">
+        <ul>
+          <li>
+            <Link className="material-link" to="/primeminister">
+              Prime Minister of India
+            </Link>
+            <button onClick={() => downloadPdf(pdf, "Prime_Minister.pdf")}>
+              Download PDF
+            </button>
+          </li>
+          <li>
+            <Link className="material-link" to="/presidents">
+              Presidents of India
+            </Link>
+            <button
+              onClick={() =>
+                downloadPdf("/pdfs/presidents.pdf", "Presidents.pdf")
+              }
+            >
+              Download PDF
+            </button>
+          </li>
+          <li>
+            <Link className="material-link" to="/emperors">
+              Emperors of India
+            </Link>
+            <button
+              onClick={() => downloadPdf("/pdfs/emperors.pdf", "Emperors.pdf")}
+            >
+              Download PDF
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
