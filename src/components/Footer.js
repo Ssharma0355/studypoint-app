5e177544aefff2a6d586e9ef97f8d7070bb1e40b
sachin_sde <ssachin.s@myclnq.co.in>
2025-02-18 17:41:45 +0530

    header-footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./Footer.css"; // Import the CSS file for styling
 
 function Footer() {
   return (
-    <div>
-      hi
+    <div className="footer">
+      <p>&copy; 2025 My Study. All rights reserved.</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,44 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css"; // Import the CSS file for styling
 
 function Header() {
   return (
-  <>
-
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-            <Link className="navbar-brand" to="/">My Study</Link>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <>
+      <nav className="navbar">
+        <div className="navbar-container">
+          <Link className="navbar-brand" to="/">
+            My Study
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-                <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-                </li>
-                <li className="nav-item">
-                <Link className="nav-link" to="/studymaterial">Study Material</Link>
-                </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/studymaterial">
+                  Study Material
+                </Link>
+              </li>
             </ul>
-            </div>
+          </div>
         </div>
-    </nav>
-
-  </>
-  )
+      </nav>
+    </>
+  );
 }
 
-export default Header
+export default Header;
